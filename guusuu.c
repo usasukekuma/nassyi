4f11509d79ff0f0cc9c20c5f2147f931c6ad05cc
Kaito <usasuke.kuma@gmail.com>
2018-05-08 21:14:06 +0900

    no message

---
+++ guusuu.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+	int data_a, amari;
+	printf("®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+	scanf_s("%d", &data_a);
+	amari = data_a % 2;
+	if (amari == 0)
+		printf("‹ô”‚Å‚·");
+	else
+		printf("Šï”‚Å‚·");
+}
+	
