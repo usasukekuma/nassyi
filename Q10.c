af0a529b48334e62532a7f3494e0b40c64e0db00
Kaito <39031086+usasukekuma@users.noreply.github.com>
2018-05-16 18:04:37 +0900

    Update Q10.c

--- Q10.c
+++ Q10.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 void main()
 {
-	int shinchou,goukei,ninzuu,average;
+	int shinchou,goukei=0,ninzuu=0,average;
 		while(shinchou!=0){
 			printf("身長を入力してください(0を入力すると終了します):");
 			scanf("%d",&shinchou);
-			goukei=shinchou+goukei;
+			goukei=goukei+shinchou;
 			++ninzuu;
 		}
-	average=goukei/ninzuu;
+	average=goukei/(ninzuu-1);
 	printf("平均は%dです。",average);
 }
