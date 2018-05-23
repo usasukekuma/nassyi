8aacaae9430984bc587c1f31bb1e2db33f54beee
Kaito <39031086+usasukekuma@users.noreply.github.com>
2018-05-23 18:00:10 +0900

    Update Q10.c

--- Q10.c
+++ Q10.c
@@ -1,18 +1,32 @@
 #include<stdio.h>
 void main()
 {
-	int joutei,katei,heigh,s;
-		while(joutei,katei,heigh!=0){
-			printf("台形の面積を求めます。整数で大きさを入力してください。(どれかに0を入力すると終了します)\n");
-            printf("上底を入力してください：");
-			scanf("%d\n",&joutei);
-            printf("下底を入力してください：");
-            scanf("%d\n",&katei);
-            printf("高さを入力してください:");
-            scanf("%d\n",&heigh);
-
+	double joutei,katei,heigh,s,x;
+		
+		printf("台形の面積を求めます。整数で大きさを入力してください。(どれかに0を入力すると終了します)\n");
+		printf("上底を入力してください：");
+		scanf("%lf",&joutei);
+		printf("下底を入力してください：");
+	    scanf("%lfd",&katei);
+		printf("高さを入力してください:");
+	    scanf("%lf",&heigh);
+		x=joutei*katei*heigh;
+		while(x!=0){
 			s=(joutei+katei)*heigh/2;
+			printf("面積は%lfです。\n",s);
+			printf("台形の面積を求めます。\n整数で大きさを入力してください。(どれかに0を入力すると終了します)\n");
+			printf("上底を入力してください：");
+			scanf("%lf",&joutei);
+			printf("下底を入力してください：");
+		    scanf("%lfd",&katei);
+			printf("高さを入力してください:");
+	    	scanf("%lf",&heigh);
+			x=joutei*katei*heigh;
 		}
+					
+		printf("終了します");
+			
+		
+	}
+			
 	
-	printf("面積は%dです。",s);
-}
