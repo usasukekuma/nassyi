eeee8730761ea1fe7b8fe1510a85750006b6349c
Kaito Oiso <usasuke.kuma@gmail.com>
2018-05-30 14:24:36 +0900

    no message

---
+++ Q11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int lunch[10],a,b;
+    double c;
+    while(lunch[a]!=0){
+        ++a;
+        printf("%d日目のランチ代を入力しましょう:",a);
+        scanf("%d",&lunch[a]);
+    }
+
+
+}
